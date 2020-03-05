bae9f24528193334ad754d5fdb49f8dbb9f4967f
cherryApp <j.cserko@gmail.com>
2020-03-05 14:44:08 +0100

    Create video_24 branch

--- public/js/db.js
+++ public/js/db.js
@@ -1,16 +1,27 @@
 // Handle firebase firestore database.
 const DB = {
     db: firebase.firestore(),
-    get: async (path) => {
-        const querySnapshot = await DB.db.collection(path).get();
+    get: async (path, where) => {
+        let ref = DB.db.collection(path);
+        ref = where ? ref.where(...where) : ref;
+        const querySnapshot = await ref.get();
         const result = {};
+
         querySnapshot.forEach((doc) => {
             result[doc.id] = doc.data();
         });
+
         return result;
     },
     create: async (path, data) => {
         const docRef = await DB.db.collection(path).add(data);
         return docRef;
+    },
+    update: async (path, data) => {
+        const docRef = DB.db.doc(`${path}`);
+        await docRef.set(data);
+    },
+    delete: async (path) => {
+        await DB.db.doc(`${path}`).delete();
     },
 };
