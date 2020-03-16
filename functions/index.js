032ac56747828e473e3a9dd6f009b31d3bd3392d
cherryApp <j.cserko@gmail.com>
2020-03-16 13:56:27 +0100

    Create video_29 branch

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,25 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+const db = admin.firestore();
+const collection = db.collection("games");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Training!");
+exports.api = functions.https.onRequest((request, response) => {
+    collection.get()
+        .then(snapshot => {
+            const results = {};
+            snapshot.forEach(doc => {
+                results[doc.id] = doc.data();
+            });
+            return response.json(results);
+        })
+        .catch(err => {
+            return response.status(400).json(err);
+        });
 });
 
